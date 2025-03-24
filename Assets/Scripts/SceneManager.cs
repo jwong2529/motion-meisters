eb8ea07a09b0b86c771f68d283a4169615343043
jwong2529 <janice.wong2529@gmail.com>
2025-03-24 12:20:01 -0700

    Connect Sony Mocopi sensors to Unity, allow live control of virtual avatar

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,21 +1,21 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+// public class SceneLoader : MonoBehaviour
+// {
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
         
-    }
+//     }
 
-    public void LoadScene(string sceneName) {
-        SceneManager.LoadScene(sceneName)
-    }
+//     public void LoadScene(string sceneName) {
+//         SceneManager.LoadScene(sceneName)
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+// }
